d858ebfb1450b03e7cfdd9d92718f2873950fb3e
Absalom H. R <hera.absalom@gmail.com>
2020-10-30 20:43:26 -0600

    Ejercicios 6 al 9

--- Sesion2/Ejercicios/Consultas.sql
+++ Sesion2/Ejercicios/Consultas.sql
@@ -1,6 +1,5 @@
 USE classicmodels;
-describe employees;
-select * from employees;
+DESCRIBE employees;
 
 # Dentro de la tabla employees, obten el número de empleado, apellido y nombre de todos los empleados cuyo nombre empiece con a.
 SELECT 
@@ -36,3 +35,35 @@ employeeNumber AS 'Número de empleado',
 lastName AS 'Apellido', 
 firstName as 'Nombre' 
 FROM employees WHERE firstName NOT LIKE 'b%';
+
+# Dentro de la tabla products, obten el código de producto y nombre de los productos cuyo código incluye la cadena _20.
+DESCRIBE products;
+SELECT productCode AS 'Código de producto', productName AS 'Nombre del producto' FROM products WHERE productCode LIKE '%_20%';
+
+# Dentro de la tabla orderdetails, obten el total de cada orden.
+DESCRIBE orderdetails;
+SELECT orderNumber AS 'Orden', SUM(priceEach * quantityOrdered) AS 'Precio otal' FROM orderdetails GROUP BY orderNumber;
+
+# Dentro de la tabla orders obten el número de órdenes por año.
+DESCRIBE orders;
+SELECT YEAR(orderDate) AS 'Año', COUNT(*) AS 'N° de órdenes' FROM orders GROUP BY YEAR(orderDate);
+
+# Obten el apellido y nombre de los empleados cuya oficina está ubicada en USA.
+DESCRIBE employees;
+SELECT * FROM offices;
+SELECT * FROM employees;
+
+# Usando el conocimiento del prework de la Sesión 3:
+SELECT lastName AS 'Apellido', firstName AS 'Nombre' FROM employees JOIN offices ON employees.officeCode = offices.officeCode WHERE offices.country = 'USA';
+
+# Obten el número de cliente, número de cheque y cantidad del cliente que ha realizado el pago más alto.
+show tables;
+desc customers;
+
+# Obten el número de cliente, número de cheque y cantidad de aquellos clientes cuyo pago es más alto que el promedio.
+
+# Obten el nombre de aquellos clientes que no han hecho ninguna orden.
+
+# Obten el máximo, mínimo y promedio del número de productos en las órdenes de venta.
+
+# Dentro de la tabla orders, obten el número de órdenes que hay por cada estado.
--- Sesion2/Ejercicios/README.md
+++ Sesion2/Ejercicios/README.md
@@ -15,21 +15,29 @@
 ### Dentro de la tabla employees, obten el número de empleado, apellido y nombre de todos los empleados cuyo nombre no inicia con B.
 ![imagen](imagenes/Consulta5.PNG)
 
-### 
-![imagen](imagenes/Consulta5.PNG)
-### 
-![imagen](imagenes/Consulta5.PNG)
-### 
-![imagen](imagenes/Consulta5.PNG)
-### 
-![imagen](imagenes/Consulta5.PNG)
-### 
-![imagen](imagenes/Consulta5.PNG)
-### 
-![imagen](imagenes/Consulta5.PNG)
-### 
-![imagen](imagenes/Consulta5.PNG)
-### 
-![imagen](imagenes/Consulta5.PNG)
-### 
-![imagen](imagenes/Consulta5.PNG)
+### Dentro de la tabla products, obten el código de producto y nombre de los productos cuyo código incluye la cadena _20.
+![imagen](imagenes/Consulta6.PNG)
+
+### Dentro de la tabla orderdetails, obten el total de cada orden.
+![imagen](imagenes/Consulta7.PNG)
+
+### Dentro de la tabla orders obten el número de órdenes por año.
+![imagen](imagenes/Consulta8.PNG)
+
+### Obten el apellido y nombre de los empleados cuya oficina está ubicada en USA.
+![imagen](imagenes/Consulta9.PNG)
+
+### Obten el número de cliente, número de cheque y cantidad del cliente que ha realizado el pago más alto.
+![imagen](imagenes/Consulta10.PNG)
+
+### Obten el número de cliente, número de cheque y cantidad de aquellos clientes cuyo pago es más alto que el promedio.
+![imagen](imagenes/Consulta11.PNG)
+
+### Obten el nombre de aquellos clientes que no han hecho ninguna orden.
+![imagen](imagenes/Consulta12.PNG)
+
+### Obten el máximo, mínimo y promedio del número de productos en las órdenes de venta.
+![imagen](imagenes/Consulta13.PNG)
+
+### Dentro de la tabla orders, obten el número de órdenes que hay por cada estado.
+![imagen](imagenes/Consulta14.PNG)
